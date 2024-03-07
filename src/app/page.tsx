16040fa641ec1b167ad166be77f9540e50faa3d2
IAmInYourKernel <soumyajit.battacharya@gmail.com>
2024-03-07 18:54:58 +0530

    Solve small problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 import Image from "next/image";
 import { Nav } from "@/app/(Components)/Nav";
 
-import PatientProfile from "./PatientProfile/page";
-import Table from "./Menucard/page";
-
-import DoctorsProfile from "./DoctorsProfile/page";
-import Services from "./Services/page";
-import Appointment from "./FollowupsChart/page";
-import MenuCard from "./Dashboard/page";
 import Login from "./LoginPage/page";
-import SignUp from "./signup/page";
-import InvoicePage from "./InvoiceAgain/page";
-import Bill from "./Bill/page";
+
 import dotenv from "dotenv";
 
 dotenv.config();
