544f044647426bfd31f49baa59b448eae0ea8280
IAmInYourKernel <soumyajit.battacharya@gmail.com>
2024-03-28 12:48:54 +0530

    fix the useEffect that runs 2 times

--- src/app/PatientProfile/page.tsx
+++ src/app/PatientProfile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PatientProfileHeader } from "../(Components)/PatientProfile/PatientData";
 import { TableCard } from "../(Components)/TableCard";
 import { Modal } from "../(Components)/Modal";
@@ -58,95 +58,99 @@ export default function PatientProfile({
   const [isLoading, setIsLoading] = useState(false);
 
   //Circular progress Indicator
+  const fetchBills = async (patientId: string) => {
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/records/get_all_bill`,
+        { patient_id: patientId },
+        {
+          withCredentials: true,
+        }
+      );
 
+      if (response.data.apiSuccess === 1) {
+        setBillPrescription(response.data.data);
+      }
+      if (response.data.apiSuccess === 0) {
+        return null;
+      }
+    } catch (error) {
+      console.error("Error fetching bills:", error);
+      handleFetchError(error);
+    }
+  };
+
+  const handleFetchError = (error: any) => {
+    // Check for network-related errors
+    if (error.code === "ECONNREFUSED" || error.code === "ENETUNREACH") {
+      setIsLoading(false);
+      setShowModal(true);
+      setMessage(
+        "There is a network issue. Please check your internet connection or contact HealthKa."
+      );
+    }
+    // Handle Axios errors
+    else if (error.response) {
+      console.log(error);
+
+      setMessage("An error occurred while fetching data.");
+      setShowModal(true);
+    }
+    // Handle other errors
+    else {
+      setIsLoading(false);
+      setMessage("An error occurred while fetching data.");
+      setShowModal(true);
+    }
+  };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get<ApiResponse>(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/patient/get_patient_by_doctor_id?patient_id=${searchParams.patient_id}`,
+        {
+          withCredentials: true,
+        }
+      );
+
+      const responseData = response.data;
+      console.log(responseData);
+      setDataLength(responseData.dataLength);
+
+      if (response.data.statusCode === 404) {
+        setMessage(responseData.message.toString());
+        setShowModal(true);
+        return;
+      }
+
+      const [patientData]: any = responseData.patientData; // Destructure the array
+
+      if (patientData) {
+        setPatient(patientData);
+        await fetchBills(patientData.patient_id);
+      }
+
+      if (responseData.data.length > 0) {
+        const patientData = responseData.data;
+        setPrescriptions(patientData);
+      }
+
+      if (responseData.data.length === 0) {
+        setMessage(responseData.message.toString());
+        setShowModal(true);
+      }
+    } catch (error: any) {
+      console.error("Error fetching patient data:", error);
+      handleFetchError(error);
+    }
+  };
+
+  let shouldReload = useRef(true);
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get<ApiResponse>(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/patient/get_patient_by_doctor_id?patient_id=${searchParams.patient_id}`,
-          {
-            withCredentials: true,
-          }
-        );
-
-        const responseData = response.data;
-        setDataLength(responseData.dataLength);
-
-        if (response.data.statusCode === 404) {
-          setMessage(responseData.message.toString());
-          setShowModal(true);
-          return; // Stop execution if patient not found
-        }
-
-        const [patientData]: any = responseData.patientData; // Destructure the array
-
-        if (patientData) {
-          setPatient(patientData);
-          await fetchBills(patientData.patient_id);
-        }
-
-        if (responseData.data.length > 0) {
-          const patientData = responseData.data;
-          setPrescriptions(patientData);
-        }
-
-        if (responseData.data.length === 0) {
-          setMessage(responseData.message.toString());
-          setShowModal(true);
-        }
-      } catch (error: any) {
-        console.error("Error fetching patient data:", error);
-        handleFetchError(error);
-      }
-    };
-
-    const fetchBills = async (patientId: string) => {
-      try {
-        const response = await axios.post(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/records/get_all_bill`,
-          { patient_id: patientId },
-          {
-            withCredentials: true,
-          }
-        );
-
-        if (response.data.apiSuccess === 1) {
-          setBillPrescription(response.data.data);
-        }
-        if (response.data.apiSuccess === 0) {
-          return null;
-        }
-      } catch (error) {
-        console.error("Error fetching bills:", error);
-        handleFetchError(error);
-      }
-    };
-
-    const handleFetchError = (error: any) => {
-      // Check for network-related errors
-      if (error.code === "ECONNREFUSED" || error.code === "ENETUNREACH") {
-        setIsLoading(false);
-        setShowModal(true);
-        setMessage(
-          "There is a network issue. Please check your internet connection or contact HealthKa."
-        );
-      }
-      // Handle Axios errors
-      else if (error.response) {
-        console.log(error);
-
-        setMessage("An error occurred while fetching data.");
-        setShowModal(true);
-      }
-      // Handle other errors
-      else {
-        setIsLoading(false);
-        setMessage("An error occurred while fetching data.");
-        setShowModal(true);
-      }
-    };
-
-    fetchData();
+    if (shouldReload.current) {
+      shouldReload.current = false;
+      console.log("Checking");
+      fetchData();
+    }
   }, [searchParams.patient_id]);
 
   // Render your component based on the patient data and data length
